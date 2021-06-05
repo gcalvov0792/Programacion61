cc8f6d3714392186574cf0269006b982f49da16e
GERALD JOSE CALVO VIDAURRE <gcalvov0792@uvirtual.ac.cr>
2021-06-04 19:22:10 -0600

    storeprocedure

---
+++ BaseDatos/dbo/StoreProcedures/MarcaVehiculoObtener.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[MarcaVehiculoObtener]
+	@MarcaVehiculoId INT= NULL
+	
+AS
+	BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+		@MarcaVehiculoId
+      ,	Descripcion
+	  , Estado
+	FROM MarcaVehiculo
+
+	END
